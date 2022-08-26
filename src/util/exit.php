84c8b2b047226a96fab10ea19793b4ab1513c522
Andrea Davanzo <info@adavanzo.com>
2022-08-26 21:28:45 +0100

    update header

--- src/util/exit.php
+++ src/util/exit.php
@@ -1,15 +1,11 @@
 <?php
-
 /* =============================================================================
- * Naranza Sesto <http://sesto.naranza.com>
- * Copyright (c) 2009-19 Andrea Davanzo
- * License BSD 3-clause. See the LICENSE file distributed with this source code.
+ * Naranza Sesto - Copyright (c) Andrea Davanzo - License MPL v2.0 - naranza.com
  * ========================================================================== */
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 class sesto_exit extends exception
 {
 
 }
-
