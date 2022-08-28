a77a9b3e2215095cde5f89482a97d223249c46f1
Andrea Davanzo <info@adavanzo.com>
2022-08-28 15:42:09 +0100

    update header

--- src/util/uuid.php
+++ src/util/uuid.php
@@ -1,12 +1,9 @@
 <?php
-
 /* =============================================================================
- * Naranza Sesto <http://sesto.naranza.com>
- * Copyright (c) 2009-19 Andrea Davanzo
- * License BSD 3-clause. See the LICENSE file distributed with this source code.
+ * Naranza Sesto - Copyright (c) Andrea Davanzo - License MPL v2.0 - naranza.com
  * ========================================================================== */
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 function sesto_uuid(): string
 {
