2fc878f2bd15ac8949ed3d29fbf1a709054af37b
Andrea Davanzo <info@adavanzo.com>
2022-10-20 17:23:45 +0100

    update header

--- src/view/render.php
+++ src/view/render.php
@@ -1,6 +1,7 @@
 <?php
+
 /* =============================================================================
- * Naranza Sesto - Copyright (c) Andrea Davanzo - License MPL v2.0 - naranza.com
+ * Naranza Sesto - Copyright (c) Andrea Davanzo - License MPL v2.0 - naranza.org
  * ========================================================================== */
 
 declare(strict_types=1);
