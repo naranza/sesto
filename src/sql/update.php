973ffda2a6b332fdc47a562d7aeff6daae75a204
Andrea Davanzo <info@adavanzo.com>
2022-10-23 22:35:22 +0100

    update header

--- src/sql/update.php
+++ src/sql/update.php
@@ -1,11 +1,10 @@
 <?php
+
 /* =============================================================================
- * Naranza Sesto <http://sesto.naranza.com>
- * Copyright (c) 2009-20 Andrea Davanzo
- * License BSD 3-clause. See the LICENSE file distributed with this source code.
+ * Naranza Sesto - Copyright (c) Andrea Davanzo - License MPL v2.0 - naranza.org
  * ========================================================================== */
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 require_once SESTO_DIR . '/sql/where.php';
 
@@ -21,4 +20,3 @@ function sesto_sql_update(string $table, array $record, array $where = null)
   }
   return $sql;
 }
-
