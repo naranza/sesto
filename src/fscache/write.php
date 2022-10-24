839b1eef8da46ba96745b5fbacd0cb2a411604f3
Andrea Davanzo <info@adavanzo.com>
2022-10-24 22:56:34 +0100

    update header

--- src/fscache/write.php
+++ src/fscache/write.php
@@ -1,9 +1,7 @@
 <?php
 
 /* =============================================================================
- * Naranza Sesto <http://sesto.naranza.com>
- * Copyright (c) 2009-19 Andrea Davanzo
- * License BSD 3-clause. See the LICENSE file distributed with this source code.
+ * Naranza Sesto - Copyright (c) Andrea Davanzo - License MPL v2.0 - naranza.org
  * ========================================================================== */
 
 declare(strict_types = 1);
@@ -11,7 +9,7 @@ declare(strict_types = 1);
 function sesto_fscache_write(string $path, string $value): bool
 {
   $result = false;
-  $fp = @fopen($path, 'w');
+  $fp = fopen($path, 'w');
   if ($fp !== false) {
     $result = fwrite($fp, $value);
     if (false !== $result) {
