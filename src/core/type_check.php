87ce654ceef4a8bb5dd89ca9a21e39cddf420163
Andrea Davanzo <info@adavanzo.com>
2022-10-26 19:57:16 +0100

    move under core

--- src/core/type_check.php
+++ src/core/type_check.php
@@ -1,19 +1,17 @@
 <?php
 
 /* =============================================================================
- * Naranza Sesto <http://sesto.naranza.com>
- * Copyright (c) 2009-19 Andrea Davanzo
- * License BSD 3-clause. See the LICENSE file distributed with this source code.
+ * Naranza Sesto - Copyright (c) Andrea Davanzo - License MPL v2.0 - naranza.org
  * ========================================================================== */
 
 declare(strict_types = 1);
 
-require_once SESTO_DIR . '/type/get.php';
+require_once SESTO_DIR . '/core/get_type.php';
 
 function sesto_type_check($expression, string $expected): string
 {
   $error = '';
-  $got = sesto_type_get($expression);
+  $got = sesto_get_type($expression);
   if ($expected !== $got) {
     $error = sprintf("Invalid type: '%s' expected '%s' got", $expected, $got);
   }
