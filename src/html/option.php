b7754b00748b9a4cc6a84beb7eb5c55399875848
Andrea Davanzo <info@adavanzo.com>
2023-12-08 08:03:58 +0000

    improve output

--- src/html/option.php
+++ src/html/option.php
@@ -6,15 +6,18 @@
 
 declare(strict_types=1);
 
-function sesto_html_option(array $options, $value): array
+require_once SESTO_DIR . '/html/element.php';
+require_once SESTO_DIR . '/html/build.php';
+
+function sesto_html_option(array $options, $value): string
 {
-  $result = [];
+  $content = "\n";
   foreach ($options as $field_value => $field_label) {
     $option_attribs = ['value' => $field_value];
     if ($field_value === $value) {
-      $option_attribs[] = 'selected';
+      $option_attribs['selected'] = 'selected';
     }
-    $result[] = sesto_html_element('option', $option_attribs, $field_label);
+    $content .= "\t" . sesto_html_build(sesto_html_element('option', $option_attribs, $field_label));
   }
-  return $result;
+  return $content;
 }
