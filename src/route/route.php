449d7e1b4e64e3b8047ace8bc59e9d8b96933420
Andrea Davanzo <info@adavanzo.com>
2026-02-18 20:25:28 +0000

    route

--- src/route/route.php
+++ src/route/route.php
@@ -1,21 +1,18 @@
 <?php
 
-/* =============================================================================
- * Naranza Sesto - Copyright (c) Andrea Davanzo - License MPL v2.0 - naranza.org
- * ========================================================================== */
+// Naranza Sesto - https://naranza.org
+// SPDX-License-Identifier: MPL-2.0
+// Copyright (c) Andrea Davanzo and contributors
 
 declare(strict_types=1);
 
 class sesto_route
 {
-
-  public string $id = '';
-  public string $url_base = '';
-  public string $url_path = '';
-  public string $url_relative = '';
+  public string $base = '';
+  public string $path = '';
+  public string $relative = '';
   public string $dirname = '';
   public string $filename = '';
   public string $basename = '';
   public string $extension = '';
-  
 }
--- src/route/solve.php
+++ src/route/solve.php
@@ -1,38 +1,33 @@
 <?php
 
-/* =============================================================================
- * Naranza Sesto - Copyright (c) Andrea Davanzo - License MPL v2.0 - naranza.org
- * ========================================================================== */
+// Naranza Sesto - https://naranza.org
+// SPDX-License-Identifier: MPL-2.0
+// Copyright (c) Andrea Davanzo and contributors
 
 declare(strict_types=1);
 
 require_once SESTO_DIR . '/route/route.php';
-require_once SESTO_DIR . '/url/base.php';
-require_once SESTO_DIR . '/url/path.php';
-require_once SESTO_DIR . '/url/relative.php';
 
-function sesto_route_solve(string $url_base = null, string $url_path = null): sesto_route
+function sesto_route_solve(?string $url_path = null, ?string $url_base = null): sesto_route
 {
   $route = new sesto_route();
-  $route->url_base = null === $url_base ? sesto_url_base() : $url_base;
-  $route->url_path = null === $url_path ? sesto_url_path() : $url_path;
-  $route->url_relative = sesto_url_relative($route->url_path, $route->url_base);
-  $len = mb_strlen($route->url_relative);
-  $char = $len > 0 ? $route->url_relative[$len - 1] : '';
-  if ('/' == $char) {
-    $pinfo = pathinfo($route->url_relative . 'index');
+  $route->path = $url_path;
+  $route->base = $url_base === null ? '' : $url_base;
+  if ($route->base !== '/') {
+    $route->relative = preg_replace('#' . $route->base . '#', '', $route->path, 1);
   } else {
-    $pinfo = pathinfo($route->url_relative);
+    $route->relative = $route->url_path;
+  }
+  $len = mb_strlen($route->relative);
+  $char = $len > 0 ? $route->relative[$len - 1] : '';
+  if ('/' == $char) {
+    $pinfo = pathinfo($route->relative . 'index');
+  } else {
+    $pinfo = pathinfo($route->relative);
   }
   $route->dirname = $pinfo['dirname'] ?? '';
   $route->basename = $pinfo['basename'] ?? '';
   $route->filename = $pinfo['filename'] ?? 'index';
   $route->extension = $pinfo['extension'] ?? '';
-  if ('/' === $route->dirname) {
-    $route->id = '/' . $route->filename;
-  } else {
-    $route->id = $route->dirname . '/' . $route->filename;
-  }
-
   return $route;
 }
